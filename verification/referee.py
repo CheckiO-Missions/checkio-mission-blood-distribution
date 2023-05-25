37194561ad9bf47117037f845a7c329d5bd5467b
retruc <97701409+retruc@users.noreply.github.com>
2023-05-25 16:48:32 +0200

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -36,7 +36,7 @@ from tests import TESTS
 def verify(answ, function):
     blood_avail, blood_needs = answ[0], answ[1]
     blood_types = ['A', 'B', 'AB', 'O']
-    distribution = function( blood_avail, blood_needs) 
+    distribution = function(answ) 
 
     
     for blood_type in blood_types:
