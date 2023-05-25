701796d75e87d7a62676d71219d79a938cea695f
retruc <97701409+retruc@users.noreply.github.com>
2023-05-25 16:35:54 +0200

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -29,6 +29,7 @@ checkio.referee.cover_codes
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
+from checkio.referees import cover_codes
 
 from tests import TESTS
 
